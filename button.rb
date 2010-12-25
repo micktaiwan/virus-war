c35f82bd5f7cf1977a13a9ff1a6876fc4be4761d
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-12-25 13:02:10 +0100

    adding missing resources

---
+++ button.rb
@@ -0,0 +1,19 @@
+# canvas button
+class Button
+
+  def initialize(file, canvas, x, y)
+    @x, @y = x, y
+    pixbuf = Gdk::Pixbuf.new(file)
+    image = Gtk::Image.new(pixbuf)
+    event_box = Gtk::EventBox.new.add(image)
+    event_box.set_visible_window(@canvas)
+    if block_given?
+      event_box.signal_connect("button_press_event") do
+        yield
+      end
+    end
+    canvas.put(event_box, @x, @y)
+  end
+
+end
+
